6ab8d92479ecec9c97916ea41cc36df3bbae0659
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-09-20 10:48:42 +0200

    Base di partenza

---
+++ Gestione_Eventi_Final/src/main/java/PetrovTodor/Gestione_Eventi_Final/GestioneEventiFinalApplication.java
@@ -0,0 +1,13 @@
+package PetrovTodor.Gestione_Eventi_Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEventiFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEventiFinalApplication.class, args);
+	}
+
+}
